f5f78a3e1335b68e034203a619252f9c7fe6721d
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2024-01-02 17:54:02 -0400

    Update auth_datasource_impl.dart and related files

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,7 +1,12 @@
-import 'package:teslo_shop/features/auth/domain/datasources/auth_datasource.dart';
-import 'package:teslo_shop/features/auth/domain/entities/user.dart';
+import 'package:dio/dio.dart';
+
+import 'package:teslo_shop/config/config.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 
 class AuthDataSourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiURl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -9,9 +14,19 @@ class AuthDataSourceImpl extends AuthDataSource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post("/auth/login", data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.usersonToEntity(response.data);
+
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
---
+++ lib/features/auth/infrastructure/mappers/user_mapper.dart
@@ -0,0 +1,11 @@
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+
+class UserMapper {
+  static User usersonToEntity(Map<String, dynamic> json) => User(
+        id: json['id'],
+        email: json['email'],
+        fullname: json['fullname'],
+        roles: List<String>.from(json['roles'].map((role) => role)),
+        token: json['token'],
+      );
+}
