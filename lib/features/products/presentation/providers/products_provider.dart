a719d4b0ad6b42d6c0609004fa3cacc3f753884b
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2024-01-03 15:51:16 -0400

    Add product state

---
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/domain/entities/product.dart';
+
+class ProductsState {
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductsState({
+    this.isLastPage = false,
+    this.limit = 10,
+    this.offset = 0,
+    this.isLoading = false,
+    this.products = const [],
+  });
+
+  ProductsState copyWith({
+    bool? isLastPage,
+    int? limit,
+    int? offset,
+    bool? isLoading,
+    List<Product>? products,
+  }) {
+    return ProductsState(
+      isLastPage: isLastPage ?? this.isLastPage,
+      limit: limit ?? this.limit,
+      offset: offset ?? this.offset,
+      isLoading: isLoading ?? this.isLoading,
+      products: products ?? this.products,
+    );
+  }
+}
+
+// final productsProvider = StateNotifierProvider<>((ref) {
+//   return 
+// });
