cedc6320f91bf7d3d67a90ffb06544b9d4ec17fc
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2024-01-03 10:14:15 -0400

    Add splash screen

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 
-class CheckAuthStatusScreen extends ConsumerWidget {
+class CheckAuthStatusScreen extends StatelessWidget {
   const CheckAuthStatusScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(authProvider, (previous, next) {
-      context.go("/");
-    });
-
+  Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(
