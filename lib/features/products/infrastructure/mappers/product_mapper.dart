2ab245acc0dc4810718a32904e0dd10d16eacec0
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2024-01-06 12:44:35 -0400

    Refactor image URL handling in ProductMapper

--- lib/features/products/infrastructure/mappers/product_mapper.dart
+++ lib/features/products/infrastructure/mappers/product_mapper.dart
@@ -15,10 +15,31 @@ class ProductMapper {
         gender: json['gender'],
         tags: List<String>.from(json['tags'].map((tag) => tag)),
         images: List<String>.from(
-          json['images'].map((img) => img.startsWith('http')
-              ? img
-              : '${Environment.apiURl}/files/product/$img'),
+          json['images'].map((img) => _imageToUrl(img)),
         ),
-        user: UserMapper.usersonToEntity(json['user']),
+        user: json['user'] != null
+            ? UserMapper.usersonToEntity(json['user'])
+            : null,
       );
+
+  static String _imageToUrl(dynamic image) {
+    String url = "";
+
+    if (image is Map) {
+      url = image['url'] ?? "";
+    }
+    if (image is String) {
+      url = image;
+    }
+
+    if (image is! String && image is! Map) {
+      throw Exception('Invalid image type');
+    }
+
+    if (url.startsWith('http')) {
+      return url;
+    }
+
+    return '${Environment.apiURl}/files/product/$url';
+  }
 }
